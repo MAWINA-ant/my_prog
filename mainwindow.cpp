e2ed1f30af483ae2cf2a5e4f98a8d6a998e414e9
Ruslan Muzhichkov <chaika1201@gmail.com>
2018-01-13 19:35:39 +0400

    nachalo ponimaniia WinApi

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-//#include "windows.h"
+#include "QDebug"
+#include "windows.h"
+#include <iostream>
+
+using namespace std;
+
+//BOOL CALLBACK EnumWindowsProc(HWND hwnd,LPARAM lParam);
+
+BOOL CALLBACK EnumWindowsProc(
+    HWND hwnd,  // handle to parent window
+    LPARAM lParam   // application-defined value
+   );
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
+    EnumWindows((WNDENUMPROC)EnumWindowsProc,(LPARAM)NULL);
+        //Sleep(1000);
+
     ui->setupUi(this);
 
 }
@@ -14,3 +30,21 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lp)
+{
+    if (IsWindowVisible(hwnd))
+    {
+        char str[255];
+        DWORD procId = NULL;
+        GetWindowText(hwnd,(LPWSTR)str,100);
+        GetWindowThreadProcessId(hwnd, &procId);
+        cout << str << endl;
+        cout << "Process -> " << procId << endl;
+        MessageBox(hwnd,(LPCWSTR)str,(LPCWSTR)"123",MB_ICONINFORMATION);
+    }
+    return true;
+}
+
+
